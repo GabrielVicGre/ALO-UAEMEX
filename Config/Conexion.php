719cae43451164008cf3fca77f4fca64827132bc
Gabriel Victoria Gregorio <88056404+GabrielVicGre@users.noreply.github.com>
2022-09-27 23:07:29 -0500

    Update Conexion.php

--- Config/Conexion.php
+++ Config/Conexion.php
@@ -15,10 +15,13 @@
     $pass = "3Fl760wnL8b@";
     $server = "sbd-alo-fi.mysql.database.azure.com";
     $db = "bd_alofi";
-    $conexion = mysqli_connect($server, $user, $pass);
+
+    //Initializes MySQLi
+    $conexion = mysqli_init();
+    mysqli_ssl_set($conexion,NULL,NULL, "../Assets/SSL/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);  
+    // Establish the connection
+    mysqli_real_connect($conexion, $server, $user, $pass, $db,3306, NULL, MYSQLI_CLIENT_SSL);
     if (mysqli_connect_errno()) {
         echo "Fallo de conexion a la base de datos";
     }
-    mysqli_select_db($conexion, $db) or die("No se encuentra la BD");
-    mysqli_set_charset($conexion, "UTF8");
 ?>
