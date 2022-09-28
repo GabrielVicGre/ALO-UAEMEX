d55659d6229605f7c56de42badb20dea2f049318
Gabriel Victoria Gregorio <88056404+GabrielVicGre@users.noreply.github.com>
2022-09-27 22:14:34 -0500

    Update Conexion.php

--- Config/Conexion.php
+++ Config/Conexion.php
@@ -1,4 +1,5 @@
 <?php 
+    /*
     $user = "root";
     $pass = "";
     $server = "localhost";
@@ -9,4 +10,15 @@
     }
     mysqli_select_db($conexion, $db) or die("No se encuentra la BD");
     mysqli_set_charset($conexion, "UTF8"); 
+    */
+    $user = "Administrador@sbd-alo-fi";
+    $pass = "3Fl760wnL8b@";
+    $server = "sbd-alo-fi.mysql.database.azure.com";
+    $db = "bd_alofi";
+    $conexion = mysqli_connect($server, $user, $pass);
+    if (mysqli_connect_errno()) {
+        echo "Fallo de conexion a la base de datos";
+    }
+    mysqli_select_db($conexion, $db) or die("No se encuentra la BD");
+    mysqli_set_charset($conexion, "UTF8");
 ?>
