e5a73eb4d74b4b4f46f9eaf65d1db3d78cac2c5f
GabrielVicGre <gabovicgre@gmail.com>
2022-10-30 22:41:04 -0600

    first commit

--- Config/IngresarPOO.php
+++ Config/IngresarPOO.php
@@ -3,28 +3,32 @@
 require '../Models/Usuario.php';
 include('../Config/ConexionPOO.php');
 
-$user = $_POST['user'];
-$password = $_POST['password'];
 
-if (isset($_POST["Ingresar"])) {
-    session_start();
-    $_SESSION['usuario'] = $user;
-    $_SESSION['password'] = $password;
+if (strlen($_POST['user'])>0 && strlen($_POST['password'])>0 ) {
+
+    $user = $_POST['user'];
+    $password = $_POST['password'];
+    
+    $sql = "SELECT * FROM administradores WHERE user like '" . $user . "' AND password like '" . $password . "'";
+    $query = $conexion->prepare($sql);
+    $query->execute();
+    $results = $query->fetchAll(PDO::FETCH_OBJ);
+
+    if ($query->rowCount() == 1) {
+    /* foreach ($results as $registro) {
+            $id = $registro->id_administrador;
+            $user = $registro->user;
+            $pass = $registro->password;
+        }*/
+        if (isset($_POST["Ingresar"])) {
+            session_start();
+            $_SESSION['usuario'] = $user;
+            $_SESSION['password'] = $password;
+        }
+        header("Location: ../Views/Administrador/inicio/index.php");
+    } else {  // No encuentra el registro
+        header("Location: ../index.php?e");     
+    }
+}else{
+    header("Location: ../index.php?i");     
 }
-
-$sql = "SELECT * FROM administradores WHERE user like '" . $user . "' AND password like '" . $password . "'";
-$query = $conexion->prepare($sql);
-$query->execute();
-$results = $query->fetchAll(PDO::FETCH_OBJ);
-
-if ($query->rowCount() == 1) {
-   /* foreach ($results as $registro) {
-        $id = $registro->id_administrador;
-        $user = $registro->user;
-        $pass = $registro->password;
-    }*/
-    header("Location: ../Views/Administrador/inicio/index.php");
-} else {  // No encuentra el registro
-     header("Location: ../index.php?e");     
-}
-
--- index.php
+++ index.php
@@ -6,9 +6,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Assets/CSS/login/index.css">
-    <link rel="stylesheet" href="Assets/CSS/login/indexResponsive.css">  
+    <link rel="stylesheet" href="Assets/CSS/login/indexResponsive.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
     <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
     <script src="Assets/Scripts/login/index.js"></script>
 </head>
@@ -32,9 +33,9 @@
                     </div>
                     <div class="containerLogin">
                         <label for="uname"><b>Usuario:</b></label>
-                        <input type="text" placeholder="correo@uaemex.mx" name="user" required>
+                        <input type="text" placeholder="correo@uaemex.mx" name="user">
                         <label for="psw"><b>Contraseña:</b></label>
-                        <input type="password" placeholder="Ingresa tu contraseña" name="password" required>
+                        <input type="password" placeholder="Ingresa tu contraseña" name="password">
                         <div class="container text-center mt-4">
                             <div class="row">
                                 <div class="col-6 p-1">
@@ -45,10 +46,19 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="errorSesion">
+                        <div class="container text-center errorSesion">
                             <?php
                             if (isset($_GET['e'])) {
-                                echo "<h6 style='color:#EC7063'> Usuario/Contraseña Incorrectos</h6>";
+                                echo "<h6 class='errorTextSesion'>
+                                        <i class='bi bi-x-circle-fill mx-2'></i>
+                                         Usuario/Contraseña Incorrectos
+                                         </h6>";
+                            }
+                            if (isset($_GET['i'])) {
+                                echo "<h6 class='errorTextSesion'>
+                                       <i class='bi bi-exclamation-triangle-fill mx-2'></i>     
+                                        Completa todos los campos
+                                         </h6>";
                             }
                             ?>
                         </div>
@@ -58,6 +68,7 @@
         </div>
     </div>
 </body>
+
 </html>
 
 <!-- GABRIEL VG -->
