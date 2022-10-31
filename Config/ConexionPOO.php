25e38215095ec525ea770af28dcb5d13deab9503
GabrielVicGre <gabovicgre@gmail.com>
2022-10-30 21:54:24 -0600

    first commit

--- Config/ConexionPOO.php
+++ Config/ConexionPOO.php
@@ -1,5 +1,7 @@
 <?php
-    
+
+/* ====================          CONEXIÓN A SERVIDOR MYSQL DE AZURE       ==================================*/
+  
     $user = "Administrador@sbd-alo-fi";
     $password = "3Fl760wnL8b@";
     $server = "sbd-alo-fi.mysql.database.azure.com";
@@ -8,5 +10,18 @@
         PDO::MYSQL_ATTR_SSL_CA => '../Assets/SSL/BaltimoreCyberTrustRoot.crt.pem'
     );
     $conexion = new PDO('mysql:host='.$server.';port=3306;dbname='.$database , $user, $password, $options);
+
+
+/* ====================          CONEXIÓN A SERVIDOR LOCAL       ==================================*/
+/* 
+$user = "root";
+$password = "";
+try {
+    $conexion = new PDO('mysql:host=localhost;dbname=bd_alofi', $user, $password);
+} catch (PDOException $e) {
+    print "¡Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+*/
 
 ?>
