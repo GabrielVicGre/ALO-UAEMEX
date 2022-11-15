3d402ac621619fa0d90b3b071068b5538179f0c2
GabrielVicGre <gabovicgre@gmail.com>
2022-11-14 18:14:33 -0600

    Cambios vistas

--- Config/database/database.sql
+++
@@ -1,7 +0,0 @@
-
-create table administradores(
-id_administrador integer primary key auto_increment,
-user varchar(30) not null,
-password varchar(30) not null, 
-nombre varchar(70) not null, 
-direccion varchar(80) not null); 
